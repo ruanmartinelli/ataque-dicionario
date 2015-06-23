35a60dbf89e53686aad36cce764be4b719668b48
BrunoAgrizzi <brunoagrizzi@gmail.com>
2015-06-23 16:54:28 -0300

    Estruturando trabalho

--- src/br/inf/ufes/pp2015_01/Master.java
+++ src/br/inf/ufes/pp2015_01/Master.java
@@ -4,7 +4,15 @@
 package br.inf.ufes.pp2015_01;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
-public interface Master extends Remote, SlaveManager, Attacker {
+public interface Master extends Remote, SlaveManager, Attacker{
 	// o mestre é um SlaveManager e um Attacker
+	
+	public  int addSlave(Slave s, String slavename) throws RemoteException;
+	public void removeSlave(int slaveKey) throws RemoteException;
+	public void foundGuess(long currentindex, Guess currentguess)throws RemoteException;
+	public void checkpoint(long currentindex) throws RemoteException;
+	public Guess[] attack(byte[] ciphertext, byte[] knowntext);
+	
 }
--- src/br/inf/ufes/pp2015_01/MasterImpl.java
+++ src/br/inf/ufes/pp2015_01/MasterImpl.java
@@ -1,23 +1,52 @@
 package br.inf.ufes.pp2015_01;
 
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MasterImpl implements Master {
 
 	private Map<String, Slave> escravos;
+	private List<Thread> threads = new ArrayList<Thread>();
 	
 	
 	public MasterImpl(){
 		escravos = new HashMap<String, Slave>();
 	}
 	
+	public static void main(String[] args) {
+		//String host = null; //(args.length < 1) ? "" : args[0];
+		//if (args.length > 0) {
+			//System.setProperty("java.rmi.server.hostname", null);//args[0]);
+		//}
+
+		//System.out.println("Connection try at host: " + host);
+
+		try {
+			MasterImpl obj = new MasterImpl();
+
+			Master ref = (Master) UnicastRemoteObject.exportObject(obj,2001);
+
+			Registry registry = LocateRegistry.getRegistry();
+			//registry.rebind("mestre", ref);
+			System.out.println("Master registered!");
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
 	
 	@Override
 	public int addSlave(Slave s, String slavename) throws RemoteException {
-		
+		synchronized(this){
 		escravos.put(slavename,s);
+		}
 		System.out.println("Escravo adicionado.");
 		
 		return 0;
@@ -44,8 +73,60 @@ public class MasterImpl implements Master {
 
 	@Override
 	public Guess[] attack(byte[] ciphertext, byte[] knowntext) {
-		// TODO Auto-generated method stub
+		List<ThreadDTO> workers = new ArrayList<ThreadDTO>();
+		
+		for (Map.Entry<String, Slave> entry : escravos.entrySet()) {
+			List<Integer> subpalavras = new ArrayList<Integer>();
+
+			
+			/* Criacao de threads. */
+			ThreadDTO exec = new ThreadDTO(entry.getValue(),subpalavras,ciphertext,knowntext);
+			workers.add(exec);
+			Thread t = new Thread(exec);
+			threads.add(t);
+
+			t.start();
+
+		}
+		for (Thread t : threads) {
+			try {
+				t.join();
+				//Thread.sleep(30000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 		return null;
+	}
+	/* Inner class para auxiliar*/
+	public class ThreadDTO extends Thread {
+
+		public final Slave escravo;
+		public List<Integer> lista;
+		public byte[] cipher;
+		public byte[] known;
+
+		public ThreadDTO(Slave es, List<Integer> lista,byte[] ciphertext, byte[] knowntext) {
+			this.cipher = ciphertext;
+			this.known = knowntext;
+			this.escravo = es;
+			this.lista = lista;
+		}
+
+		/* Executa quando a thread eh iniciacada. */
+		@Override
+		public void run() {
+
+			try {
+				
+				System.out.println("");
+				lista = escravo.startSubAttack(cipher,known,/*long com o indice inicial*/,/*long com o indice final*/,/*função de callback*/);
+				
+				
+			} catch (RemoteException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
